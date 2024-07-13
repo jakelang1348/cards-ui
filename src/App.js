90522b0ac5daff35bf0b5603d121dd43d5007a2e
jakelang <jakelang@umich.edu>
2024-07-13 12:32:36 -0500

    basic hand layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const cards = [
+  "Lorem ipsum dolor sit amet",
+  "Consectetur adipiscing elit",
+  "Sed do eiusmod tempor",
+  "Incididunt ut labore et dolore",
+  "Magna aliqua. Ut enim ad minim",
+  "Lorem ipsum dolor sit amet",
+  "Consectetur adipiscing elit",
+  "Sed do eiusmod tempor",
+  "Incididunt ut labore et dolore",
+  "Magna aliqua. Ut enim ad minim"
+
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Your Hand</h1>
+        <div className="hand">
+          {cards.map((card, index) => (
+            <div key={index} className="card">
+              {card}
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
